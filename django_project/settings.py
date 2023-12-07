308521c99d328fa98db418351bd993d3c93e80b3
Anish Bade <badeanish6045@gmail.com>
2023-12-07 23:41:29 +0545

    added pre-commit feature

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "todos.apps.TodosConfig", #new
+    "todos.apps.TodosConfig",  # new
 ]
 
 MIDDLEWARE = [
